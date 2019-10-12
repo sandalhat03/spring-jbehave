2cfaccbedd2c4d89d774ab762c380421691d658a
sandalhat03 <fsolijon@gmail.com>
2019-10-13 01:48:10 +0800

    Initial Commit

---
+++ src/main/java/com/fps/springjbehave/SpringJbehaveApplication.java
@@ -0,0 +1,13 @@
+package com.fps.springjbehave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJbehaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJbehaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fps/springjbehave/SpringJbehaveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fps.springjbehave;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJbehaveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
